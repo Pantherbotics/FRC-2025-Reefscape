a249225d1de7b03b79c16397498679f4387d9f50
soysauce2393 <hubblesoren@gmail.com>
2025-04-12 16:43:28 -0700

    stuff

--- src/main/java/frc/robot/subsystems/Indexer/Indexer.java
+++ src/main/java/frc/robot/subsystems/Indexer/Indexer.java
@@ -6,10 +6,10 @@ package frc.robot.subsystems.Indexer;
 
 import com.ctre.phoenix6.hardware.TalonFX;
 
+import edu.wpi.first.units.measure.Current;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants.GroundPivotConstants;
 import frc.robot.Constants.IndexerConstants;
 
 public class Indexer extends SubsystemBase {
@@ -22,6 +22,11 @@ public class Indexer extends SubsystemBase {
   public Command setVoltage(double voltage){
     return this.runOnce(()->m_rollerMotor.setVoltage(voltage)).andThen(Commands.idle(this));
   }
+
+  public Current motorCurrent(){
+    return m_rollerMotor.getStatorCurrent().getValue();
+  }
+
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
